dad95e7a7a6c4bc22e536811e1a646ce0d7caf7a
cluster327 <cluster327@outlook.com>
2022-10-18 10:05:41 +0800

    m

--- RWKV-v4/src/model.py
+++ RWKV-v4/src/model.py
@@ -380,7 +380,7 @@ class GPT(nn.Module):
         return optimizer
 
     def forward(self, idx, targets=None):
-        idx = idx.to(self.emb.weight.device)
+        idx = idx.to(self.emb.weight.device).half()
 
         self.step += 1
         B, T = idx.size()
